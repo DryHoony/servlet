a455fdaefb071a93e7c8fbdb2d6c2a0225389602
Hoony <xognsdl710@naver.com>
2023-04-16 21:58:44 +0900

    2023.04.16 <4. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술> 2) 서블릿

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
+	// <4. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술>
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
